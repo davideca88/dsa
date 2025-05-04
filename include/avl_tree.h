393cb131196df7cb051ec0b679b168d748997615
Davideca <lopes.270206@gmail.com>
2025-05-04 16:56:37 -0400

    Cabeçalho de árvore avl criado

---
+++ include/avl_tree.h
@@ -0,0 +1,82 @@
+#ifndef _AVL_TREE_H
+#define _AVL_TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define max(A, B) ((A > B) ? A : B)
+
+// Nó para árvore avl
+typedef struct _avl_node_s {
+    struct _avl_node_s* l;
+    struct _avl_node_s* r;
+    uint32_t data;
+    uint8_t height;
+} AvlNode;
+
+typedef AvlNode* Avl;
+
+// Inicializa uma árvore avl
+Avl new_avl();
+
+// Insere na árvore avl
+Avl avl_insert(Avl t, uint32_t data);
+
+// Busca a subárvore que possui data
+Avl avl_search(Avl t, uint32_t data);
+
+// Retorna o tamanho da árvore
+uint8_t avl_height(Avl t);
+
+#endif // _AVL_TREE_H
+/*
+
+//Avl avlinsert(Avl t, uint32_t data) {
+//    return _avlinsert(t, data, 0);
+//}
+   
+Avl avl_l_rotation(Avl t) {
+    Avl aux;
+    aux = t->r;
+    t->r = aux->l;
+    aux->l = t;
+    return aux;
+}
+Avl avl_l_rotation(Avl t) {
+    Avl aux;
+    
+    aux = t->r;
+    t->r = aux->r;
+    aux->r = t->l;
+    t->l = aux;
+
+    return t;
+}
+
+Avl avl_r_rotation(Avl t) {
+    Avl aux;
+    
+    aux = t->l;
+    t->l = aux->l;
+    aux->l = t->r;
+    t->r = aux;
+    
+    return t;
+}
+
+Avl avl_rl_rotation(Avl t) {
+    avl_r_rotation(t->r);
+    avl_l_rotation(t);
+
+    return t;
+}
+
+Avl avl_lr_rotation(Avl t) {
+    avl_l_rotation(t->l);
+    avl_r_rotation(t);
+
+    return t;
+}
+
+*/
---
+++ src/avl_tree.c
@@ -0,0 +1,120 @@
+#include "../include/avl_tree.h"
+
+AvlNode _NullNode = {
+    NULL,
+    NULL,
+    0,
+    (int8_t) -1
+};
+
+Avl NullNode = &_NullNode;
+
+Avl new_avl() {
+    return NullNode;
+}
+
+uint8_t avl_height(Avl t) {
+    return t->height;
+}
+
+void avl_update_height(Avl t) {
+    uint8_t hl = t->l->height;
+    uint8_t hr = t->r->height;
+
+    t->height = 1 + max(hl, hr);
+}
+
+int8_t avl_balance_factor(Avl t) {
+    return (int8_t) (t->l->height - t->r->height);
+}
+
+Avl avl_r_rotate(Avl t) {
+    Avl aux;
+    
+    aux = t->l;        
+    t->l = aux->r;
+    aux->r = t;
+
+    avl_update_height(t);
+    avl_update_height(aux);
+    
+    return aux;
+}
+
+Avl avl_l_rotate(Avl t) {
+    Avl aux;
+
+    aux = t->r;
+    t->r = aux->l;
+    aux->l = t;
+    
+    avl_update_height(t);
+    avl_update_height(aux);
+        
+    return aux;
+}
+
+Avl avl_balance(Avl t) {
+    avl_update_height(t);
+
+    int8_t bf = avl_balance_factor(t);
+
+    if(bf > 1) {
+        if(avl_balance_factor(t->l) < 0) {
+            t->l = avl_l_rotate(t->l);
+        }
+        return avl_r_rotate(t);
+    }
+
+    if(bf < -1) {
+        if(avl_balance_factor(t->r) > 0) {
+            t->r = avl_r_rotate(t->r);
+        }
+        return avl_l_rotate(t);
+    }
+
+    return t;
+}
+
+Avl avl_insert(Avl t, uint32_t data) {
+
+    if(t == NullNode) {
+        Avl new = (Avl) malloc(sizeof(AvlNode));
+        new->l = NullNode;
+        new->r = NullNode;
+        new->data = data;
+        new->height = 0;
+         
+        return new;
+    }
+
+    if(data < t->data) {
+        t->l = avl_insert(t->l, data);
+    }
+    
+    else if(data > t->data) {
+        t->r = avl_insert(t->r, data);
+    }
+    
+    else {
+        return t;
+    }
+
+    return avl_balance(t);
+}
+
+Avl avl_search(Avl t, uint32_t data) {
+    if(t == NullNode)
+        return NULL;
+
+    if(data < t->data) {
+        return avl_search(t->l, data);
+    }
+    else if(data > t->data) {
+        return avl_search(t->r, data);
+    }
+
+    else {
+        return t;
+    }
+}
