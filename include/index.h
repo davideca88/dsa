0c039fbb4300a4015175b9525a0cf356340c5f0c
Davideca <lopes.270206@gmail.com>
2025-06-12 16:45:59 -0400

    Avanços no cabeçalho de Índices

--- include/index.h
+++ include/index.h
@@ -2,6 +2,7 @@
 #define _INDEX_H
 
 #include "hash_table.h"
+#include "product.h"
 //#include "avl_tree.h"
 
 #include <stdio.h>
@@ -14,32 +15,72 @@
 #define SIZEOF_FIELD(type, field) sizeof(((type*)0)->field)
 
 #define HASH_TABLE 3
-#define AVL        4
+#define AVL_EQ     4
+#define AVL_RANGE  5
 
 typedef struct _index_s* Index;
 
 struct _index_s {
-    FILE* rec_fd;
-    void* idx_p;
-    char idx_mode;
-    Offset (*search)(Index idx, Key key);
-    bool (*insert)(Index idx, Key key, Offset offset);
-    void (*destroy)(Index idx);
-    void (*print)(Index idx);
+    FILE*   rec_fd;
+    void*   idx_p;
+    char    mode;
+    Product (*search)(Index idx, Key key);
+    Product*(*query)(Index idx, const char* fint, Price fprice, const char* lint, Price lprice);
+    bool    (*insert)(Index idx, Key key, Offset offset);
+    bool    (*build)(Index idx);
+    void    (*clear_query)(Product* query);
+    void    (*print)(Index idx);
 };
+
+inline bool build(Index idx) {
+    Product product;
+    Offset offset = 0;
+    rewind(idx->rec_fd);
+    
+    switch(idx->mode) {
+        case HASH_TABLE:
+            HashTable ht = (HashTable) idx->idx_p;
+
+            while(fread(&product, sizeof(Product), 1, idx->rec_fd)) {
+                ht->insert(ht, product.id, offset);
+                offset = ftell(idx->rec_fd);
+            }
+
+            return true;
+        break; // HASH_TABLE
+    }
+    return false;
+}
 
 inline bool hash_table_insert(Index idx, Key key, Offset offset) {
     return ((HashTable) idx->idx_p)->insert(idx->idx_p, key, offset);
 }
 
-inline Offset hash_table_search(Index idx, Key key) {
-    return ((HashTable)idx->idx_p)->get(idx->idx_p, key);
+inline Product hash_table_search(Index idx, Key key) {
+    Offset offset = ((HashTable)idx->idx_p)->get(idx->idx_p, key);
+    Product product = {
+        .description = '\0',
+        .name = '\0',
+        .id = 0,
+        .price = 0,
+        .quantity = 0
+    };
+    
+    fread(&product, sizeof(Product), 1, idx->rec_fd);
+    
+    return product;
 }
+
+inline Product avl_eq_search(Index idx, Key key);
+
+inline bool avl_insert(Index idx, Key key, Offset offset);
 
 Index create_index(const char* rec_name, const char* key_mode, size_t len, ...) {
     Index idx = (Index) malloc(sizeof(struct _index_s));
     
     FILE* rec_fd = fopen(rec_name, "wb+");
+    
+    idx->build = build;
     
     if(!strcmp(key_mode, "price")) {
         
@@ -53,11 +94,12 @@ Index create_index(const char* rec_name, const char* key_mode, size_t len, ...) 
         switch(arg) {
             case HASH_TABLE:
                 idx->idx_p = new_hash_table(len, CHAINING);
+                idx->mode = HASH_TABLE;
                 idx->search = hash_table_search;
                 idx->insert = hash_table_insert;
                 
             break; // HASH_TABLE
-            case AVL:
+            case AVL_EQ:
                 
             break; // AVL
             
