e5ada571f1fca492e399d635ba4d934dc7b47ee5
Davideca <lopes.270206@gmail.com>
2025-08-19 21:59:10 -0400

    Mudanças

--- include/utils.h
+++ include/utils.h
@@ -7,21 +7,22 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
  * check_allocation - checa se uma syscall de alocação de memória falhou
  *
- * @ptr:        retorno da função — syscall — de alocação 
+ * @ptr:        retorno da função — syscall — de alocação
+ * @err_msg:    mensagem de erro personalizada (opcional)
  *
  * Se a alocação falhar, encerrará o programa imediatamente e exibirá uma
- * mensagem em stderr
+ * mensagem em stderr. No caso de não oferecer mensagem de erro, preencher com
+ * NULL
  */
-static inline void check_allocation(void *ptr) {
-    if(ptr == NULL) {
-        perror("Heap failed");
+static inline void check_allocation(void *ptr, const char* err_msg) {
+    if(ptr == NULL) { 
+        perror(err_msg ? err_msg : "Heap failed");
         exit(EXIT_FAILURE);
     }
 }
